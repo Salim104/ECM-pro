de612ab310952c65604b14273a6d89a4091c0011
salim104 <salimshaban885@gmail.com>
2022-06-05 19:52:14 +0200

    added

--- index.js
+++ index.js
@@ -10,44 +10,44 @@ navbarEl.addEventListener('click', function() {
 })
 
 // slideshow
-const buttons = document.querySelectorAll("[data-carousel-button]")
+// const buttons = document.querySelectorAll("[data-carousel-button]")
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]")
+// buttons.forEach(button => {
+//   button.addEventListener("click", () => {
+//     const offset = button.dataset.carouselButton === "next" ? 1 : -1
+//     const slides = button
+//       .closest("[data-carousel]")
+//       .querySelector("[data-slides]")
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+//     const activeSlide = slides.querySelector("[data-active]")
+//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
+//     if (newIndex < 0) newIndex = slides.children.length - 1
+//     if (newIndex >= slides.children.length) newIndex = 0
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
-})
+//     slides.children[newIndex].dataset.active = true
+//     delete activeSlide.dataset.active
+//   })
+// })
 
 
 // another
 
-const slideshowImages = document.querySelectorAll(".slide");
+// const slideshowImages = document.querySelectorAll(".slide");
 
-const nextImageDelay = 5000;
-let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
+// const nextImageDelay = 5000;
+// let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
 
-// slideshowImages[currentImageCounter].style.display = "block";
-slideshowImages[currentImageCounter].style.opacity = 1;
+// // slideshowImages[currentImageCounter].style.display = "block";
+// slideshowImages[currentImageCounter].style.opacity = 1;
 
-setInterval(nextImage, nextImageDelay);
+// setInterval(nextImage, nextImageDelay);
 
-function nextImage() {
-  // slideshowImages[currentImageCounter].style.display = "none";
-  slideshowImages[currentImageCounter].style.opacity = 0;
+// function nextImage() {
+//   // slideshowImages[currentImageCounter].style.display = "none";
+//   slideshowImages[currentImageCounter].style.opacity = 0;
 
-  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
+//   currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
 
-  // slideshowImages[currentImageCounter].style.display = "block";
-  slideshowImages[currentImageCounter].style.opacity = 1;
-}
+//   // slideshowImages[currentImageCounter].style.display = "block";
+//   slideshowImages[currentImageCounter].style.opacity = 1;
+// }
