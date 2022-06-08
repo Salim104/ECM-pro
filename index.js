cac305d4bb8df03e6aad01a479904e86001f3498
salim104 <salimshaban885@gmail.com>
2022-06-08 23:05:21 +0200

    color

--- index.js
+++ index.js
@@ -32,22 +32,22 @@ navbarEl.addEventListener('click', function() {
 
 // another
 
-// const slideshowImages = document.querySelectorAll(".slide");
+const slideshowImages = document.querySelectorAll(".slide");
 
-// const nextImageDelay = 5000;
-// let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
+const nextImageDelay = 5000;
+let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
 
-// // slideshowImages[currentImageCounter].style.display = "block";
-// slideshowImages[currentImageCounter].style.opacity = 1;
+// slideshowImages[currentImageCounter].style.display = "block";
+slideshowImages[currentImageCounter].style.opacity = 1;
 
-// setInterval(nextImage, nextImageDelay);
+setInterval(nextImage, nextImageDelay);
 
-// function nextImage() {
-//   // slideshowImages[currentImageCounter].style.display = "none";
-//   slideshowImages[currentImageCounter].style.opacity = 0;
+function nextImage() {
+  // slideshowImages[currentImageCounter].style.display = "none";
+  slideshowImages[currentImageCounter].style.opacity = 0;
 
-//   currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
+  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
 
-//   // slideshowImages[currentImageCounter].style.display = "block";
-//   slideshowImages[currentImageCounter].style.opacity = 1;
-// }
+  // slideshowImages[currentImageCounter].style.display = "block";
+  slideshowImages[currentImageCounter].style.opacity = 1;
+}
